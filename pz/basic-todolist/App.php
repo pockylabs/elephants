57ce8f02caf36a82caf2a5185f6155baa26ea937
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2023-01-08 06:29:22 +0700

    determine view

--- pz/basic-todolist/App.php
+++ pz/basic-todolist/App.php
@@ -4,6 +4,9 @@ require_once "Model/Todolist.php";
 require_once "BusinessLogic/ShowTodolist.php";
 require_once "BusinessLogic/AddTodolist.php";
 require_once "BusinessLogic/RemoveTodolist.php";
+require_once "View/ViewShowTodolist.php";
+require_once "View/AddShowTodolist.php";
+require_once "View/RemoveShowTodolist.php";
 
 echo "Aplikasi TodoList" . PHP_EOL;
 
