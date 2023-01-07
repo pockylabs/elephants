ddb29d27c8eb55c1349bea0ef549101fba0c1567
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2023-01-08 06:31:57 +0700

    show todolist

--- pz/basic-todolist/BusinessLogic/ShowTodolist.php
+++ pz/basic-todolist/BusinessLogic/ShowTodolist.php
@@ -3,5 +3,11 @@
 // Menampilkan todo di List
 
 function showTodoList() {
+    global $todoList;
 
+    echo "TODOLIST" . PHP_EOL;
+
+    foreach($todoList as $number => $value) {
+        echo "$number. $value" . PHP_EOL;
+    }
 }
