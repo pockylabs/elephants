13aeb1446968c7254fce6690f1a7940316f199e4
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2023-01-08 06:41:45 +0700

    test show todolist

---
+++ pz/basic-todolist/Test/TestShowTodolist.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../Model/Todolist.php";
+require_once "../BusinessLogic/ShowTodolist.php";
+
+$todoList[1] = "Learn Basic PHP";
+$todoList[2] = "Learn OOP PHP";
+$todoList[3] = "Learn PHP Database";
+
+showTodoList();
